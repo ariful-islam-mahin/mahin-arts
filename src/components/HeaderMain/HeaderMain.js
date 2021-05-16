218cda0da18112d72181a7b18a6cccd4c599b2b7
ariful-islam-mahin <mahinyayaa@gmail.com>
2021-05-17 00:54:35 +0600

    picture fix

--- src/components/HeaderMain/HeaderMain.js
+++ src/components/HeaderMain/HeaderMain.js
@@ -4,7 +4,7 @@ const HeaderMain = () => {
     return (
         <div>
             <header class="w3-display-container w3-wide" id="home">
-                <img class="w3-image" src="https://lh3.googleusercontent.com/23uMOo_NuljfZWolwhyTTq2_ReCr74jDlGmZMUlUE4E5qykxDcaWTiDW1eqEGlFMZJEljQe_UfQjI2f0VdcWxwmJ37KCrxy-puzt5RIZ5qzDUNnxeLfKG5uzR0EY3w8m52PawqnkrhTY6XesVseJMPat-a2sU_ouQ7gQADfiGozHqgBMFgj9AyodbZCMwlFpaswPuqX7kiebct4_PQQpmCJisjF9WIukB0WQwOMWWp20m6DdtIFmEVLxkYVR2Lj_5vTm7_Qb18vDZQl2eOT9SHNExDuhDqaMXz8Q4M3gYWuFigNEzVPO7M28YrEStV89nTDtX8b1fVOvh7fO8jfKd_9sGkKhWfUBkGHf4UEdGbeqcxwK7aHdPYQqtayB9grlD4Tg8NTBmjpSkxbZ_dTC0iKI7bZx_rawQLXBNi5WMSAetxChqhaiA6t_W0du7eQnAx8IAtia5xgdHaFrxUTwmVeU_eb6kXb2kWuR_4InCch_8m5FPQIw2LJkIap8NiG3CEV351kQOyJ8dVt_oXyP1jljvhGfSK49FLD6w64-3bchW7osMTqBN3CAuRsx9Ypv7CtmZq_VZ5Z9ZPKgQ6lWCdfdJYpMkTcea7Rb3I-6uHn350JYmOxt7oL8SCLQleEziGin98TK1D_7jG0thy0Oi_qvEEiHVu3VN17sq279_V2elVNQP_KJIC_YIhEf-9Dn9Ix6R7JBN-wXTbj9f89NwsiW=w1294-h869-no?authuser=1" alt="Sketch Processing" width="1600" height="1060"/>
+                <img class="w3-image" src="https://i.ibb.co/SvNYvKY/IMG-0735.jpg" alt="Sketch Processing" width="1600" height="1060"/>
                 <div class="w3-display-left w3-padding-large">
                 <h1 class="w3-text-white ">Mahin <span class="w3-text-black">Arts</span></h1>
                 </div>
